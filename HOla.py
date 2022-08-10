601ec594ebe8828b71d9f7b10df1817be4217434
Lilliana Uribe <lillianauribe@gmail.com>
2022-08-09 23:24:07 -0500

    ejemplo de ejercicio------

---
+++ HOla.py
@@ -0,0 +1,11 @@
+print("hola mundo hermoso")#ejemplo de comentario
+#mi primer comentario
+"""multi comentario en varias lineas
+esto es para un ejemplo"""
+ana="este es un texto"
+edad= 20 # entero
+estatura=1.70 # punto para que sea decimal 
+positivo =True
+negativo= False 
+print(type(ana)) # esto asi indica el tipo de dato
+
